46832510832b99f58fd2d45a030b039de496bce4
jessicakenney <jessicakenney@yahoo.com>
2017-09-22 12:59:17 -0700

    new animal component

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,7 @@ import { Animal } from './animal.model';
     <h1> Animal Tracker</h1>
     <h3> {{currentZoo}}</h3>
     <h4> {{month}}/{{day}}/{{year}}</h4>
+    <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
     <animal-list [childAnimalList] = "masterAnimalList" (clickSender)="editAnimal($event)"> </animal-list>
     <edit-animal [childSelectedAnimal] = "selectedAnimal" (editDoneSender)="doneEdit()"> </edit-animal>
   </div>
@@ -23,15 +24,18 @@ export class AppComponent{
 
   masterAnimalList: Animal[] = [
     new Animal("elephant", "Sammy",5,"herbivore","Elephant House",5,"male","red bouncy ball", "other male elephants"),
-    new Animal("orangatang", "Flower",5,"herbivore","Monkey Pavilion",5,"female","rope swinging", "music"),
+    new Animal("orangatang", "Flower",1,"herbivore","Monkey Pavilion",5,"female","rope swinging", "music"),
   ];
   selectedAnimal = null;
 
-  editAnimal(clickedAnimal){
+  editAnimal(clickedAnimal: Animal){
     this.selectedAnimal = clickedAnimal;
   }
   doneEdit(){
     this.selectedAnimal = null;
+  }
+  addAnimal(newAnimal: Animal){
+    this.masterAnimalList.push(newAnimal);
   }
 
 }
---
+++ app/new-animal.component.ts
@@ -0,0 +1,52 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'new-animal',
+  template: `
+  <div *ngIf="!showNewAnimalForm">
+    <button (click)="newAnimalButtonClicked()">Add a New Animal</button><hr>
+  </div>
+  <div *ngIf="showNewAnimalForm" class="newAnimalForm">
+    <h1>New Animal</h1>
+    <div>
+       <label>Enter Animal Species:</label>
+       <input #newSpecies> <br>
+       <label>Enter Animal Name:</label>
+       <input #newName> <br>
+       <label>Enter Animal Age:</label>
+       <input #newAge> <br>
+       <label>Enter Animal Diet:</label>
+       <input #newDiet> <br>
+       <label>Enter Animal location:</label>
+       <input #newLocation> <br>
+       <label>Enter Animal numCare:</label>
+       <input #newNumCare> <br>
+       <label>Enter Animal Sex:</label>
+       <input #newSex> <br>
+       <label>Enter Animal Likes:</label>
+       <input #newLikes> <br>
+       <label>Enter Animal Dislikes:</label>
+       <input #newDislikes> <br>
+       <button (click)="submitForm(newSpecies.value, newName.value, newAge.value, newDiet.value, newLocation.value, newNumCare.value, newSex.value, newLikes.value, newDislikes.value); newSpecies.value=''; newName.value=''; newAge.value=''; newDiet.value=''; newLocation.value=''; newNumCare.value=''; newSex.value=''; newLikes.value=''; newDislikes.value=''; ">Admit New Animal</button>
+       <br>
+     </div>
+   </div>
+  `
+})
+
+export class NewAnimalComponent {
+  @Output() newAnimalSender = new EventEmitter();
+  showNewAnimalForm : boolean;
+
+  submitForm (newSpecies: string, newName: string, newAge: number, newDiet: string, newLocation: string, newNumCare: number,newSex: string,newLikes: string, newDislikes: string){
+    var newAnimalToAdd: Animal = new Animal(newSpecies, newName, newAge, newDiet,newLocation,newNumCare,newSex,newLikes,newDislikes);
+    this.newAnimalSender.emit(newAnimalToAdd)
+    this.showNewAnimalForm = false;
+  }
+
+  newAnimalButtonClicked(){
+      this.showNewAnimalForm = true;
+  }
+
+}
